d1d8638ac780adfcb3365c2d02ed9277fba6f386
camayal <camayal@gmail.com>
2021-10-22 19:15:39 -0400

    added nexus compatibility

--- alignment2hdf5.py
+++ alignment2hdf5.py
@@ -159,7 +159,7 @@ def _read_fasta(fp):
     #iterate line by line in file
     for line in fp:
         #remove returns and new lines
-        line = line.rstrip()
+        line = line.strip()
         
         #if line start with > it consider it a header,
         #but if header already exists, it means that previous
@@ -183,9 +183,6 @@ def _read_fasta(fp):
     if header: 
         yield (header, seq, index)
         
-
-def _read_nexus(fp):
-    pass
 
 
 def _build_hdf5(phy, phynames, phymap, scaffold_names, scaffold_lengths, output):
@@ -220,6 +217,15 @@ def split_fasta_to_hdf5(fasta, number_loci=1, output=None):
     sample_1: ACGGCAC and GTAAAGTA
     sample_2: ACTGCAC and GTAGGGTA
 
+    Parameters:
+    fasta (string)
+    Path to fasta file to be converted.
+
+    number_loci (integer)
+    Number of final loci that will be produced. Default: 1
+
+    output (string)
+    Name of the file and extension of the final produced file. By default it is used the same name of the fasta file.
     """
     # create empty arrays
     phymap = []
@@ -291,10 +297,6 @@ def split_fasta_to_hdf5(fasta, number_loci=1, output=None):
 
 def multiple_fastas_to_hdf5(list_fastas, output="alignment.hdf5"):
     """
-    Parameters:
-    list_fastas (string)
-    Path where fastas are located, it can be something like: "./aligned/*.fna" or a list ["gene1.fa", "gene2.fa, gene3.fa"]
-    
     Convert multiple gene alignments (one fasta per gene or locus) into one HFD5 file compatible with ipyrad tools and Superbpp. 
 
     Giving two fastas for each loci:
@@ -317,6 +319,13 @@ def multiple_fastas_to_hdf5(list_fastas, output="alignment.hdf5"):
     sample_2: ACTGCACGTAGGGTA
     sample_3: ACTGCAANNNNNNNN
     
+    Parameters:
+    list_fastas (string)
+    Path where fastas are located, it can be something like: "./aligned/*.fna" or a list ["gene1.fa", "gene2.fa, gene3.fa"]
+    
+    output (string)
+    Name of the file and extension of the final produced file. Default: "alignment.hdf5"
+
     """
     #load the first and create a dictionary with sample name as key and start appending genes
     #this is an alignment, so it is expected be square
@@ -403,13 +412,182 @@ def multiple_fastas_to_hdf5(list_fastas, output="alignment.hdf5"):
 
 
 
-def nexus_to_hdf5(nexus, output=None, extract_other_info=True, quiet=False, force=False):
-    """aaa"""
-    #formats: nloci, 
-  
-    pass
+def _read_nexus(nexus):
+    """Function generator that returns block and content of the block by lines
+    given a nexus file (sequential or interleaved)
+    """
+    block = None
+    keywords = ["charpartition", "taxpartition"]
+
+    for line in nexus:
+        if len(line) > 1:
+            line = line.strip().lower()
+
+            #Detect start comments 
+            if line.startswith("["):
+                block = "comment"
+
+            #Detect if comment close, even in the same line or future lines
+            #and remove block
+            if line.endswith("]"):
+                block = None
+                continue
+
+            #Skip all info if block is a comment
+            if block == "comment":
+                continue
+
+            #compatibility with execute command, extract info from other file
+            #mostly used to separate command nexus from matrix nexus
+            if line.startswith("execute"):
+                data_nexus = line.split()[1].replace(";","")
+                with open(data_nexus, "r") as fp:
+                    for ex_line in _read_nexus(fp):
+                        yield ex_line
+
+            ##
+            ## Identify blocks
+            ##
+
+            #Detect the keyword matrix, enter in seqs block skip to next line
+            if line.startswith("matrix"):
+                block = "seqs"
+                continue
+ 
+            #Detect the keywords enter in proper block
+            #and remove keywords, names and operands from actual lines
+            for keyword in keywords:
+                if line.startswith(keyword):
+                    block = keyword
+                    line = re.sub(f"{keyword}.*=", "", line)
+
+            ## Yield info inside blocks (removing single semicolons)                
+            if block and len(line) > 2:
+                yield block, line
+
+            #close block when semicolon is present
+            if line.endswith(";"):
+                block = None
+
+
+
+def nexus_to_hdf5(nexus, output=None, extract_other_info=True):
+    """
+    Convert nexus alignment into HFD5 file compatible with ipyrad tools and Superbpp.
+    
+    This function assumes matrix is square (same length for all samples), and requires
+    data and charpartition blocks. Other blocks will be ignored.  Data block could have
+    a sequential or interleaved DNA matrix. However 'matchchar' parameter is not supported.
+    This function can use 'execute' command in the nexus file to load data and charpartition 
+    blocks from other nexus files.
+
+    Example of file.nex:
+        #NEXUS
+        [This is an example of nexus file]
+
+        Begin data;
+            Dimensions ntax=6 nchar=48;
+            Format datatype=nucleotide gap=- missing=?;
+            Matrix
+        a1    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+        a2    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+        b1    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+        b2    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+        c1    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+        c2    CTGATTTACATGTCAGATGTTTTTACTAGTTCCCAACAGTTTCTCATG
+            ;
+        End;
+
+        [charpartition block is requiered]
+        charpartition lociset =
+        1: 1-10,
+        2: 11-20,
+        3: 21-30,
+        4: 31-40,
+        5: 41-48;
+        end;
+
+
+    Parameters:
+    fasta (string)
+    Path to fasta file to be converted.
+
+    extract_other_info (boolean)
+    Extract taxpartition block and convert into an IMAP file. Default: True
+
+    output (string)
+    Name of the file and extension of the final produced file. By default it is used the same name of the nexus file.
+    """
+    dataset = {}
+
+    # create empty arrays
+    phymap = []
+    scaffold_names = []
+    scaffold_lengths = []
+    phynames = []
+    phy = []
+    number_loci = 0
+
+    with open(nexus) as fp:
+        for block, line in _read_nexus(fp):
+            #fill dataset dict with sequences
+            if block == "seqs":
+                name_sample, seq = line.split()
+                if name_sample in dataset:
+                    dataset[name_sample] += seq
+                else:
+                    dataset[name_sample] = seq
+                
+
+            if block == "charpartition":
+                # idx_locus + 1, start, end, 0, end]
+                number_loci += 1
+                locus, range = (i.strip() for i in line.split(":"))
+                start, end = re.sub(",|;|\s", "", range).split("-") #remove any remaining separator and split the string
+                phymap.append([number_loci, int(start), int(end), 0, int(end)])
+                scaffold_names.append(f"loc-{locus}")
+                scaffold_lengths.append((int(end)-int(start))+1)
+                
+
+            if block == "taxpartition" and extract_other_info:
+                pass
+
+    #use dataset to populate phynames and phy
+    for sample in dataset:
+        phynames.append(sample)
+        phy.append([0 if base in ["-","?"] else ord(base) for base in dataset[sample].strip().upper()])
+    
+    
+    #create hdf5 file
+        #define default hdf5 path if is not provided
+        if not output:
+            path = os.path.dirname(nexus)
+            base = os.path.basename(nexus)
+            name = os.path.splitext(base)[0]
+            output = os.path.join(path, name + ".hdf5")
+        _build_hdf5(phy, phynames, phymap, scaffold_names, scaffold_lengths, output)
+
+
 
 
 
 if __name__ == "__main__":
-    multiple_fastas_to_hdf5("*.FNA")
+    # #test with simple nexus
+    # with open("nexus.nex") as fp:
+    #     for line in _read_nexus(fp):
+    #         print(line)
+
+    # #test with nexus that is linked to a data matrix in another file
+    # with open("nexuslink.nex") as fp:
+    #     for line in _read_nexus(fp):
+    #         print(line)
+
+    # #test with interleaving
+    # with open("nexusinterleaved.nex") as fp:
+    #     for line in _read_nexus(fp):
+    #         print(line)
+
+
+    nexus_to_hdf5("nexus.nex")
+    # nexus_to_hdf5("nexuslink.nex")
+    # nexus_to_hdf5("nexusinterleaved.nex")
