97b6d0d7fb5036c48c8ea11e0d303ccfc79998c5
camayal <camayal@gmail.com>
2021-10-22 20:40:08 -0400

    added nexus compatibility

--- alignment2hdf5.py
+++ alignment2hdf5.py
@@ -5,147 +5,6 @@ import os
 import glob
 import re
 
-
-# def convert(fasta, nloci=None, ns=None, hdf5=None, quiet=False):
-#     """
-#     DEPRECATED: Expanded compatibility with multi and single line fastas
-#     Convert fasta alignments into HFD5 file compatible with ipyrad tools. 
-#     It is capable of spliting sequences into multiple loci by size or by Ns chains.
-    
-#     fasta (path)
-#     Fasta file with alignment to convert
-    
-#     nloci (integer)
-#     Number of loci to split secuence.
-#     Example: given a a sequence like: ACTGCACGTAGGGTA and nloci=2
-#     fasta2hdf5 will consider two loci: ACTGCAC and GTAGGGTA
-    
-#     ns (integer)
-#     Number of Ns that denotate the end of a locus and the begining of another.
-#     Example: given a sequence like: ACTGCACGNNNNNNNNNNTAGGGTA and ns=10
-#     fasta2hdf5 will consider two loci: ACTGCACG and TAGGGTA
-    
-#     hdf5 (path)
-#     Path to save the resulting HDF5 file. Default=Same location and name as fasta
-#     """
-    
-#     print("""-----------------------------------------------------------------------------------------------------------
-#     DEPRECATION NOTICE: This function is deprecated, use instead: split_fasta_to_hdf5() or multiple_fastas_to_hdf5()
-#     -----------------------------------------------------------------------------------------------------------
-#     """)
-    
-#     if nloci and ns:
-#         raise Exception("Only one mode is allowed not both. 1) arbitrarily split the sequence in N loci or 2) Ns as locus separator")
-#     elif not nloci and not ns:
-#         raise Exception("Define the method to delimitate loci from sequences with nloci OR ns")
-        
-#     #define default hdf5 path
-#     if not hdf5:
-#         path = os.path.dirname(fasta)
-#         base = os.path.basename(fasta)
-#         name = os.path.splitext(base)[0]
-#         hdf5 = os.path.join(path, name + ".hdf5")
-        
-#     with open(fasta) as f:
-        
-#         phynames = []
-#         phy = []
-
-#         for idx, line in enumerate(f):
-#             #if line is a header extract the first element before space as name
-#             if line[0] == ">":
-#                 phynames.append(line.split()[0][1:])
-
-#             # else extract the sequence info
-#             else:
-#                 #Mode arbitrary n loci
-#                 if nloci:
-#                     # if is the first sequence create phymap and scaffold dataset
-#                     if idx == 1:
-                       
-#                         # create empty arrays
-#                         phymap = []
-#                         scaffold_names = []
-#                         scaffold_lengths = []
-
-#                         # get length
-#                         length = len(line.strip())
-
-#                         ## if nloci is provided 
-#                         if nloci > length:
-#                             raise Exception(f"Impossible to get the number of loci requested ({nloci}), the number is larger than bases in the alignment ({length})")
-
-#                         length_loci = length / nloci
-#                         adjusted_length_loci = math.floor(length_loci)
-
-#                         # split each locus one by one
-#                         for idx_locus in range(nloci):
-#                             start = idx_locus*adjusted_length_loci
-#                             end = start + adjusted_length_loci
-
-
-#                             # if length is not divisible, include the remainder bases in the last locus
-#                             if idx_locus == range(nloci)[-1] and end < length:
-#                                 end += length%nloci
-
-
-#                             # fill phymap, scaffold_lengths, and scaffold_names 
-#                             phymap.append([idx_locus + 1, start, end, 0, end])
-#                             scaffold_names.append(f"loc-{idx_locus + 1}")
-#                             scaffold_lengths.append(end-start)
-
-#                     # prepare phy, for now add sequence by sequence to the file. 
-#                     phy.append([0 if base in ["N","-","?"] else ord(base) for base in line.strip().upper()])
-                    
-                    
-#                     # certainly this will fill the memory try somethng like:
-#                     #   def append(self, values):
-#                     #     with h5py.File(self.datapath, mode='a') as h5f:
-#                     #         dset = h5f[self.dataset]
-#                     #         dset.resize((self.i + 1, ) + shape)
-#                     #         dset[self.i] = [values]
-#                     #         self.i += 1
-#                     #         h5f.flush()
-
-#                 #Mode loci separated by NNNNN chains
-#                 if ns:
-#                     # if is the first sequence create phymap and scaffold dataset
-#                     if idx == 1:
-
-#                         # create empty arrays
-#                         phymap = []
-#                         scaffold_names = []
-#                         scaffold_lengths = []
-
-#                         #get location of loci                       
-#                         for idx_locus, locus in enumerate(re.finditer("[^=]+", line.strip().upper().replace("N"*ns,"="))):
-#                             start = locus.start() - idx_locus
-#                             end = locus.end() - idx_locus
-
-                            
-#                             phymap.append([idx_locus + 1, start, end, 0, end])
-#                             scaffold_names.append(f"loc-{idx_locus + 1}")
-#                             scaffold_lengths.append(end-start)
-
-#                     phy.append([0 if base in ["N","-","?"] else ord(base) for base in line.strip().upper().replace("N"*ns,"")])
-
-
-
-#         with h5py.File(hdf5, 'w') as h:
-#             h["phy"] = np.asarray(phy, dtype="u1")
-#             h["phymap"] = np.asarray(phymap)
-#             h["scaffold_names"] = np.asarray(scaffold_names, dtype="S")
-#             h["scaffold_lengths"] = np.asarray(scaffold_lengths)
-#             h["phymap"].attrs["reference"] = "imported-from-fasta"
-#             h["phymap"].attrs["phynames"] = np.asarray(phynames, dtype="S")
-#             h["phymap"].attrs["columns"] = [b"chroms", b"phy0", b"phy1", b"pos0", b"pos1",]
-        
-#         if not quiet: 
-#             print(f"HDF5 file saved at: {hdf5}")
-
-
-
-
             
 def _read_fasta(fp):
     """Function generator that returns headers, sequences, and index one by one
@@ -544,9 +403,11 @@ def nexus_to_hdf5(nexus, output=None, extract_other_info=True):
                 number_loci += 1
                 locus, range = (i.strip() for i in line.split(":"))
                 start, end = re.sub(",|;|\s", "", range).split("-") #remove any remaining separator and split the string
-                phymap.append([number_loci, int(start), int(end), 0, int(end)])
+                start = int(start) - 1 
+                end = int(end)
+                phymap.append([number_loci, start, end, 0, end])
                 scaffold_names.append(f"loc-{locus}")
-                scaffold_lengths.append((int(end)-int(start))+1)
+                scaffold_lengths.append(end-start)
                 
 
             if block == "taxpartition" and extract_other_info:
@@ -588,6 +449,6 @@ if __name__ == "__main__":
     #         print(line)
 
 
-    nexus_to_hdf5("nexus.nex")
+    nexus_to_hdf5("./test/nexus.nex")
     # nexus_to_hdf5("nexuslink.nex")
     # nexus_to_hdf5("nexusinterleaved.nex")
